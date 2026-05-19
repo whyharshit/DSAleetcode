f7dafc43d76a63f02aabb6429567429809ef8377
whyharshit <pinkuverma2375@gmail.com>
2026-05-20 00:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (94.86%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,25 +11,18 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, int targetSum,int sum,int& val){
-          if(!root) return;
-
-          sum += root->val;
-          if(!(root->left) && !(root->right) && sum ==targetSum){
-            val = 1;
-            return;
-          }
-          helper(root->left,targetSum,sum,val);
-          helper(root->right,targetSum,sum,val);
-
-
-    }
+ 
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int val=0;
-        int w =0;
-        helper(root,targetSum,0,val);
-        if(val==1) return true;
-        else return false;
+        if(!root) return false;
+        int sum = targetSum-root->val;
+        targetSum=sum;
+       
+        if(!(root->left) && !(root->right) && sum==0) return true;
+        
+        return hasPathSum(root->left,targetSum)||hasPathSum(root->right,targetSum);;
+        
+        
+        
 
         
          
